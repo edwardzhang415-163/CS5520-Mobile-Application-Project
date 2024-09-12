ec6f9ff9cd7b93d3f27a311870617027fd04bbe8
edwardzhang415-163 <yXU HDU@LAPTOP-T3OV2S1Q MINGW64 ~$ git config --global user.email>
2024-09-12 11:22:22 -0700

    Update App.js to display a welcome message with the app name

--- Trailer/App.js
+++ Trailer/App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = 'MyApp';
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
