4729733a626bc6b02ac0c14016b0bddbd98648df
edwardzhang415-163 <yXU HDU@LAPTOP-T3OV2S1Q MINGW64 ~$ git config --global user.email>
2024-10-10 18:59:36 -0700

    Refactor GoalItem component to add long press functionality for deleting goals

--- Trailer/components/GoalItem.js
+++ Trailer/components/GoalItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable,Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import PressableButton from './PressableButton';
 import { colors, commonStyles } from '../styles';
@@ -7,9 +7,28 @@ import { MaterialIcons } from '@expo/vector-icons'; // Import MaterialIcons
 
 export default function GoalItem({ item, onDelete }) {
   const navigation = useNavigation();
+
+  const handleLongPress = () => {
+    Alert.alert(
+      "Delete Goal",
+      "Are you sure you want to delete this goal?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "Delete",
+          onPress: () => onDelete(item.id)
+        }
+      ]
+    );
+  };
+
   return (
     <Pressable
       onPress={() => navigation.navigate('Details', { goal: item })}
+      onLongPress={handleLongPress}
       style={({ pressed }) => (pressed ? [commonStyles.goalItem, commonStyles.goalItemPressed] : commonStyles.goalItem)}
       android_ripple={{ color: colors.ripple, borderless: false }}
     >
