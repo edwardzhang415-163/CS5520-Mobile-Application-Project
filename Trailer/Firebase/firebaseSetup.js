1e691ba44eacd52b1913f14fe74f37ff09c37e1a
edwardzhang415-163 <yXU HDU@LAPTOP-T3OV2S1Q MINGW64 ~$ git config --global user.email>
2024-10-17 11:41:51 -0700

    Refactor firebaseSetup.js to add Firebase SDKs and initialize analytics

--- Trailer/Firebase/firebaseSetup.js
+++ Trailer/Firebase/firebaseSetup.js
@@ -1,15 +1,23 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCYmDQnbotqY9tiwm6GZ_YbPbuH2t6hfmU",
+  authDomain: "edward-44c28.firebaseapp.com",
+  projectId: "edward-44c28",
+  storageBucket: "edward-44c28.appspot.com",
+  messagingSenderId: "24032945572",
+  appId: "1:24032945572:web:a8f636c681e44af475cd31",
+  measurementId: "G-Y0WG641G0D"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default app;
